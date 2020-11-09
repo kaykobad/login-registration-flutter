ee94ce84940f01b08ab258952e75bcdade705b8e
kaykobad <kaykobadreza@gmail.com>
2020-11-09 09:09:56 +0600

    Add color constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  static const Color PrimaryDark = Color(0xFFFEDBD0);
+  static const Color PrimaryLight = Color(0xFFFEEAE6);
+  static const Color HeadLine = Color(0xFF442C2E);
+  static const Color TextColor = Color(0xFF616161);
+}
