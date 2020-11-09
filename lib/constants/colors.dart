7847715ac628530472512ef2a8596057de2a663a
kaykobad <kaykobadreza@gmail.com>
2020-11-09 10:23:43 +0600

    Add login, registration and welcome page

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,11 +2,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ThemeColor {
-  static const Color PrimaryDark = Color(0xFFFEDBD0);
-  static const Color PrimaryLight = Color(0xFFFEEAE6);
+  static const Color PrimaryDark = Color(0xFF6F35A5);
+  static const Color PrimaryLight = Color(0x996F35A5);
   static const Color HeadLine = Color(0xFF442C2E);
   static const Color TextColor = Color(0xFF616161);
-
-  // const kPrimaryColor = Color(0xFF6F35A5);
-  // const kPrimaryLightColor = Color(0xFFF1E6FF);
 }
